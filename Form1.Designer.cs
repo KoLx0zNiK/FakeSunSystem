7982a0f48fc14ce7941ec0662acaa8f68bcd08c7
Neo <Neo@DESKTOP-H6916IN>
2021-02-20 10:33:03 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace SunSistem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(219, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1536, 867);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Меркурий",
+            "Венера",
+            "Земля",
+            "Марс",
+            "Юпитер",
+            "Сатурн",
+            "Уран",
+            "Нептун"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(154, 157);
+            this.checkedListBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Года";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 134);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1767, 892);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SunSistem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public double _t, _dt, _speedMerc, _speedVen, _speedEarth, _speedMars, _speedUpit, _speedSat, _speedUran, _speedNept;
+        public void Drow()//рисует солнышко
+        {
+            Graphics system = pictureBox1.CreateGraphics();
+            system.FillEllipse(Brushes.Yellow, 400, 300, 50, 50);
+        }
+        public void ShowRes()//в Текст боксе должен показывать таймер
+        {
+            textBox1.Text = _t.ToString();
+        }
+        private void Planet()//рисует планеты, если на них галочка, убирает, если ее нет
+        {
+            checkedListBox1.CheckOnClick = true;
+            if (checkedListBox1.GetItemCheckState(0) == CheckState.Checked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.DarkGray, 395, 295, 5, 5);
+            }
+            if (checkedListBox1.GetItemCheckState(1) == CheckState.Checked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.YellowGreen, 375, 275, 20, 20);
+
+            }
+            if (checkedListBox1.GetItemCheckState(2) == CheckState.Checked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.LightSkyBlue, 350, 250, 25, 25);
+            }
+            if (checkedListBox1.GetItemCheckState(3) == CheckState.Checked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.OrangeRed, 338, 238, 12, 12);
+            }
+            if (checkedListBox1.GetItemCheckState(4) == CheckState.Checked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Orange, 288, 188, 50, 50);
+            }
+            if (checkedListBox1.GetItemCheckState(5) == CheckState.Checked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.BlueViolet, 243, 143, 45, 45);
+            }
+            if (checkedListBox1.GetItemCheckState(6) == CheckState.Checked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.DeepSkyBlue, 211, 111, 32, 32);
+            }
+            if (checkedListBox1.GetItemCheckState(7) == CheckState.Checked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Blue, 181, 81, 30, 30);
+            }
+            if (checkedListBox1.GetItemCheckState(0) == CheckState.Unchecked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Black, 395, 295, 5, 5);
+            }
+            if (checkedListBox1.GetItemCheckState(1) == CheckState.Unchecked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Black, 375, 275, 20, 20);
+
+            }
+            if (checkedListBox1.GetItemCheckState(2) == CheckState.Unchecked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Black, 350, 250, 25, 25);
+            }
+            if (checkedListBox1.GetItemCheckState(3) == CheckState.Unchecked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Black, 338, 238, 12, 12);
+            }
+            if (checkedListBox1.GetItemCheckState(4) == CheckState.Unchecked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Black, 288, 188, 50, 50);
+            }
+            if (checkedListBox1.GetItemCheckState(5) == CheckState.Unchecked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Black, 243, 143, 45, 45);
+            }
+            if (checkedListBox1.GetItemCheckState(6) == CheckState.Unchecked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Black, 211, 111, 32, 32);
+            }
+            if (checkedListBox1.GetItemCheckState(7) == CheckState.Unchecked)
+            {
+                Graphics system = pictureBox1.CreateGraphics();
+                system.FillEllipse(Brushes.Black, 181, 81, 30, 30);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//недотаймер, Жень помоги
+        {
+            Drow();
+            Planet();
+            _t += _dt;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)// по нажатию кнопки должно все заработать
+        {
+            Drow();
+            Planet();
+            ShowRes();
+        }
+    }
+}
